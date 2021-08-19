35b791c7d405ad2fb8e0a70202e73b7bb259d59d
Julia2303 <julia.kazakova23@gmail.com>
2021-08-19 21:53:23 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = expr.match(/.{10}/g);
+    const dot = '.';
+    const dash = '-';
+    let morseArray = [];
+    for (let item of array) {
+        let symbol = '';       
+        for(let i = 0; i < item.length; i += 2) {
+            if(item.substr(i, 2) === '10') {
+                symbol += dot;
+            } else if(item.substr(i, 2) === '11') {
+                symbol += dash;
+            } 
+        }
+        if (item.includes('*')) {
+            symbol = ' ';
+        }
+        morseArray.push(symbol);
+    }
+    let result = [];
+    for (let item of morseArray) {
+        if(item === ' ') {
+            result.push(' ');
+        } else {
+            result.push(MORSE_TABLE[item]);
+        }
+    }
+    return result.join('');
 }
 
 module.exports = {
     decode
 }
+
+console.log(decode('00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010'))
